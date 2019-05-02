5a59f579a17d9e11b9ffd5c71e7cd512ca4689a6
Sandeep <>
2019-05-03 06:37:13 +1000

    Graphs : Machines problem solved.

--- Machines.cs
+++ Machines.cs
@@ -4,90 +4,83 @@ using System.Linq;
 
 namespace Hackerrank
 {
-    public class Town
-    {
-        public int Id { get; set; }
-        public List<Tuple<int,int>> Connections { get; set; } = new List<Tuple<int,int>>();
-        public bool HasMachine { get; set; }
-        public Town(int id)
-        {
-            Id = id;
-        }
-    }
-
     public static class Machines
     {
         public static int minTime(int[][] roads, int[] machines)
         {
-            List<Town> cityList = new List<Town>();
 
-            for (int i = 0; i < roads.Length + 1; i++)
-            {
-                var t = new Town(i);
-                if (Array.IndexOf(machines, i) != -1)
-                {
-                    t.HasMachine = true;
-                }
-
-                cityList.Add(new Town(i));
-            }
-
-            foreach (var road in roads)
-            {
-                var source = road[0];
-                var destination = road[1];
-                var cost = road[2];
-                cityList[source].Connections.Add(Tuple.Create(destination,cost));
-                cityList[destination].Connections.Add(Tuple.Create(source,cost));
-            }
-
+            var connections = new Dictionary<int, int>();
             int minCost = 0;
-            for (int m1 = 0; m1 < machines.Length; m1++)
+
+            for (int i = 0; i <= roads.Length; i++)
             {
-                for (int m2 = m1 + 1; m2 < machines.Length; m2++)
+                connections.Add(i, i);
+            }
+
+            var orderedRoads = roads.OrderByDescending(c => c[2]).ToArray();
+
+            for (int i = 0; i < orderedRoads.Length; i++)
+            {
+                var src = orderedRoads[i][0];
+                var dest = orderedRoads[i][1];
+
+                if (DestroyRoad(connections, machines, src, dest))
                 {
-                    minCost += CalculateCheapestRoad(machines[m1], machines[m2], ref cityList);
+                    minCost += orderedRoads[i][2];
                 }
             }
 
             return minCost;
         }
 
-        static int CalculateCheapestRoad(int source, int destination, ref List<Town> cities)
+        private static bool DestroyRoad(Dictionary<int, int> connections,
+        int[] machines,
+        int src, int dest)
         {
-            var minCost = 0;
-            var linksource = 0;
-            var linkDest = 0;
-            var bfs = new Queue<Tuple<int,int>>();
-            bfs.Enqueue(new Tuple<int,int>(source,int.MaxValue));
-            var currentMinRoadCost = int.MaxValue;
-            while (bfs.Count() != 0)
-            {
-                var tuple = bfs.Dequeue();
-                var pathfrom = tuple.Item1;
-
-                var sourceTownConnections = cities[pathfrom].Connections;
-
-                currentMinRoadCost = Math.Min(tuple.Item2, currentMinRoadCost);
-                if(currentMinRoadCost >= tuple.Item2)
-                {
-                    currentMinRoadCost = tuple.Item2;
-                    linksource = tuple.Item1;
-                }
-
-                foreach (var connection in sourceTownConnections)
-                {
-                    if(connection.Item1 == destination)
-                    {
-                        minCost =  Math.Min(currentMinRoadCost,connection.Item2);
-                        return minCost;
-                    }
-                    bfs.Enqueue(new Tuple<int,int>(connection.Item1, connection.Item2));                    
-                }
-            }
-
-            return minCost; 
+            if(HasMachines(connections, machines, src) && HasMachines(connections ,machines, dest))
+                return true;
+            Union(connections, machines, src, dest);
+            return false;
         }
+
+        private static bool HasMachines(Dictionary<int, int> connections
+        , int[] machines, int src)
+        {
+            if(machines.Contains(src))
+             return true;
+            var root = FindRoot(connections,src);
+            return machines.Contains(root);
+        }
+
+        private static int FindRoot(Dictionary<int, int> connections, int vertex)
+        {
+            if(connections[vertex] == vertex)
+                return vertex;
+            
+            return FindRoot(connections, connections[vertex]);
+        }
+
+        private static void Union(Dictionary<int, int> connections,
+            int[] machines,
+            int source, int desti)
+        {
+            var src = FindRoot(connections,source);
+            var dest = FindRoot(connections, desti);
+
+            if (machines.Contains(src))
+            {
+                connections[dest] = src;
+            }
+            else if (machines.Contains(dest))
+            {
+                connections[src] = dest;
+            }
+            else
+            {
+                connections[dest] = src;
+            }
+        }
+
     }
 
 }
